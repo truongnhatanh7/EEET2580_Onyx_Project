e7a8fe6239209ea7f54ee9e9c5d54fe01107bdd3
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-04 12:54:41 +0700

    Add "Add new list" feature

--- app/assets/js/controller.js
+++ app/assets/js/controller.js
@@ -1,18 +1,18 @@
-'use strict'
-const addListBtn = $('.workspace__add-list-btn')
-const board = $('.workspace__board')
+"use strict";
+const addListBtn = $(".workspace__add-list-btn");
+const addListBtnWrapper = $(".workspace__add-list-wrapper");
+const board = $(".workspace__board");
 
-
-addListBtn.addEventListener('click', () => {
-    console.log('yes')
-    let html =       `  <div class="workspace__board-list">
-    <h1 class="workspace__board-list-header" contenteditable="true">List 1</h1>
-    <div class="workspace__board-list-task" draggable="true">
-        <p class="workspace__board-list-task-content">velit3 </p>
+addListBtn.addEventListener("click", () => {
+  let html = `  
+    <div class="workspace__board-list" ondragover="handleDragOver(event)">
+        <h1 class="workspace__board-list-header" contenteditable="true">List 1</h1>
+        <div class="workspace__board-list-task" draggable="true" ondragstart="handleDragStart(event)" ondragend="handleDragEnd(event)">
+            <p class="workspace__board-list-task-content">velit3 </p>
+        </div>
     </div>
-</div>`
-    let para = document.createRange().createContextualFragment(html)
-    board.appendChild(para)
-})
+    `;
 
-
+  let para = document.createRange().createContextualFragment(html);
+  board.insertBefore(para, addListBtnWrapper);
+});
--- app/assets/js/dragndrop.js
+++ app/assets/js/dragndrop.js
@@ -1,57 +1,98 @@
-'use strict'
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-const draggables = $$('.workspace__board-list-task')
-const containers = $$('.workspace__board-list')
+"use strict";
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const draggables = $$(".workspace__board-list-task");
+const containers = $$(".workspace__board-list");
 
+// draggables.forEach((draggable) => {
+//   draggable.addEventListener("dragstart", () => {
+//     // handleDragStart(draggable);
+//     draggable.classList.add("workspace__board-list-task--dragging");
+//   });
 
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('workspace__board-list-task--dragging')
-        console.log('drag start')
+//   draggable.addEventListener("dragend", () => {
+//     //   handleDragEnd(draggable);
+//     draggable.classList.remove("workspace__board-list-task--dragging");
+//   });
 
-    })
+  
+// });
 
-    
+function handleDragStart(event) {
+    event.target.classList.add("workspace__board-list-task--dragging");
+    console.log("start")
+}
 
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('workspace__board-list-task--dragging')
+function handleDragEnd(event) {
+    event.target.classList.remove("workspace__board-list-task--dragging");
+}
 
-    })
-})
-
-containers.forEach(container => {
-    container.addEventListener('dragover', e => {
-        e.preventDefault()
-        const afterElement = getDragAfterElement(container, e.clientY)
-
-        const draggable = $('.workspace__board-list-task--dragging')
+function handleDragOver(event) {
+    const container = event.target
+    event.preventDefault();
+    if ((container.classList['0'] === 'workspace__board-list')) {
+        const afterElement = getDragAfterElement(container, event.clientY);
+        const draggable = $(".workspace__board-list-task--dragging");
         if (afterElement == null) {
-            container.appendChild(draggable)
+          container.appendChild(draggable);
         } else {
-            container.insertBefore(draggable, afterElement)
+          container.insertBefore(draggable, afterElement);
         }
-    })
-
-})
-
-function getDragAfterElement(container, yMousePos) {
-    // Use y because the boundaries are in current container
-
-    const draggableElements = [...container.querySelectorAll('.workspace__board-list-task:not(.workspace__board-list-task--dragging)')]  // Convert node list to array
-
-    return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect() // Get current box
-        
-        const offset = yMousePos - box.top - box.height / 2 // If mouseY near center of current child -> negative value but we need the negative one AND closest to 0
-        if (offset < 0 && offset > closest.offest) {
-            return { offset: offset, element: child }
-        } else {
-            return closest
-        }
-    }, { offest: Number.NEGATIVE_INFINITY }).element
-
+    }
 }
 
 
 
+// function handleDragStart(event) {
+//     console.log(typeof event.target)
+// }
+
+// containers.forEach((container) => {
+//   container.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//     const afterElement = getDragAfterElement(container, e.clientY);
+//     const draggable = $(".workspace__board-list-task--dragging");
+    
+//     if (afterElement == null) {
+//       container.appendChild(draggable);
+//     } else {
+//       container.insertBefore(draggable, afterElement);
+//     }
+//   });
+// });
+
+// function handleDragOver(event) {
+//     event.preventDefault();
+//     const afterElement = getDragAfterElement(event.currentTarget, event.clientY);
+//     console.log(typeof event.currentTarget) 
+//     const draggable = $(".workspace__board-list-task--dragging");
+//     if (afterElement == null) {
+//       event.target.appendChild(draggable);
+//     } else {
+//       event.target.insertBefore(draggable, afterElement);
+//     }
+// }
+
+function getDragAfterElement(container, yMousePos) {
+  // Use y because the boundaries are in current container
+
+  const draggableElements = [
+    ...container.querySelectorAll(
+      ".workspace__board-list-task:not(.workspace__board-list-task--dragging)"
+    ),
+  ]; // Convert node list to array
+
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect(); // Get current box
+
+      const offset = yMousePos - box.top - box.height / 2; // If mouseY near center of current child -> negative value but we need the negative one AND closest to 0
+      if (offset < 0 && offset > closest.offest) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offest: Number.NEGATIVE_INFINITY }
+  ).element;
+}
