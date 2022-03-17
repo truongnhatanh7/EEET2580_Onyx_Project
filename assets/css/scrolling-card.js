82a6d2cadecad30f1fb1a0778416285a4da49a79
Tran Mai Nhung <nhungmaitran1412@gmail.com>
2022-03-17 14:23:11 +0700

    DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE all html

--- assets/css/scrolling-card.js
+++ assets/css/scrolling-card.js
@@ -1,6 +1,16 @@
 
 let scrollContent = $('#content');
 let cards = $('.card');
+let cardBackgrounds = $('.card-content-illus');
+
+let cardIllustrationUrls = [
+    "/assets/img/homepage-content-cards/dragdrop.png",
+    "/assets/img/homepage-content-cards/collab.png"
+];
+for (let i = 0; i < cardIllustrationUrls.length; i ++) {
+    $(cardBackgrounds[i]).css("background-image", 'url("' + cardIllustrationUrls[i] + '")');
+}
+
 
 let yPosiScroll = $(cards[0]).offset().top;
 console.log(yPosiScroll);
