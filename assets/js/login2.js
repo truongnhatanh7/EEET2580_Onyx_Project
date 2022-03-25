eba7309955673aa279ca1beacc82e32a165f0b59
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-25 13:52:27 +0700

    Update login and workspace

--- assets/js/login2.js
+++ assets/js/login2.js
@@ -2,12 +2,13 @@ const loginSwitch = $('#login-switch')
 const signUpSwitch = $('#sign-up-switch')
 const loginForm = $('.sign-in-wrapper')
 const signUpForm = $('.sign-up-wrapper')
+const returnHome = $$('.return-home')
 
 signUpSwitch.addEventListener('click', () => {
-        loginForm.classList.add('disable')
-        loginForm.classList.remove('enable')
-        signUpForm.classList.remove('disable')
-        signUpSwitch.classList.add('enable')
+    loginForm.classList.add('disable')
+    loginForm.classList.remove('enable')
+    signUpForm.classList.remove('disable')
+    signUpSwitch.classList.add('enable')
 })
 
 loginSwitch.addEventListener('click', () => {
@@ -16,3 +17,10 @@ loginSwitch.addEventListener('click', () => {
     signUpForm.classList.add('disable')
     signUpSwitch.classList.remove('enable')
 })
+
+
+for (returnHomeBtn of returnHome) {
+    returnHomeBtn.addEventListener('click', () => {
+        location.assign("/index.html")
+    })
+}
