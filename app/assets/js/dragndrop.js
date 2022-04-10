2603cfb14d306407ada6dc072c60f8b8b7f38e6b
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-04-10 11:56:49 +0700

    Update dragndrop.js
    Change logic to PUT

--- app/assets/js/dragndrop.js
+++ app/assets/js/dragndrop.js
@@ -4,59 +4,57 @@ const $$ = document.querySelectorAll.bind(document);
 const draggables = $$(".workspace__board-list-task");
 const containers = $$(".workspace__board-list");
 
-
 function handleDragStart(event) {
-    
     event.target.classList.add("workspace__board-list-task--dragging");
-    sessionStorage.setItem("currentTask", event.target.id.slice(5))
-  sessionStorage.setItem('isEditing', '1')
-
+    sessionStorage.setItem("currentTask", event.target.id.slice(5));
+    sessionStorage.setItem("isEditing", "1");
 }
 
 function handleDragEnd(event) {
-  sessionStorage.setItem('isEditing', '0')
 
     event.target.classList.remove("workspace__board-list-task--dragging");
-    if (sessionStorage.getItem("currentTask") != event.target.parentNode.id) { // Avoid same list
-      let deleteTaskUrl =
-          "http://localhost:8080/api/v1/task/" + event.target.id.slice(5)
+        let taskContent = event.target.textContent.trim();
 
-      let taskContent = event.target.textContent.trim();
 
-      fetch(deleteTaskUrl, {
-          cache: "no-cache",
-          method: "DELETE",
-          headers: {
-              "Content-Type": "application/json",
-              "Cache-Control": "no-cache",
-          },
-      });
-      console.log("\n///////MOVE//////\n")
-      console.log("delete task invoked")
+        console.log("\n///////MOVE//////\n");
+        let parent = event.target.parentNode;
+        let listId = parent.id.slice(5);
+        if (event.target.parentNode.id.slice(5) == "") {
+            // If event.target return board-list-scrollable
+            parent = parent.parentNode;
+            listId = parent.id.slice(5); // Change it to the outer parent
+        }
+        
+        let moveTaskUrl = "http://localhost:8080/api/v1/task/switchList/" + listId;
+        fetch(moveTaskUrl, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                taskId: event.target.id.slice(5)
+            })
+        })
+        
+        let allTasks = parent.querySelectorAll('.workspace__board-list-task');
+        let cur = 0;
+        let setPosUrl = "http://localhost:8080/api/v1/task/setPos/"
+        allTasks.forEach(task => {
+            fetch(setPosUrl + task.id.slice(5) + "/" + cur, {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            cur++;
+        })
+        
 
-      let listId = event.target.parentNode.id.slice(5)
-      if (event.target.parentNode.id.slice(5) == '') { // If event.target return board-list-scrollable
-            listId = event.target.parentNode.parentNode.id.slice(5) // Change it to the outer parent
-      }
-          let createTaskUrl =
-              "http://localhost:8080/api/v1/task/" + listId;
-                fetch(createTaskUrl, {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                  taskContent: taskContent,
-              }),
-          }).then(response => response.json())
-          .then(data => {
-            console.log("create task invoked\n/////////\n")
-            event.target.id = "task_" + data.taskId.toString();
-          })
- 
 
-    }
 
+    sessionStorage.setItem("isEditing", "0");
+
+    // }
 }
 
 function handleOnDrag(event) {
@@ -66,25 +64,27 @@ function handleOnDrag(event) {
 function handleDragOver(event) {
     // const containerFake = $('.workspace__board-list.modifying');
     // const container = containerFake.querySelector(".workspace__board-list-scrollable")
-    let container = event.target.parentNode
+    let container = event.target.parentNode;
     event.preventDefault();
     if (container.classList["0"] === "workspace__board-list-scrollable") {
         const afterElement = getDragAfterElement(container, event.clientY);
         let draggable = $(".workspace__board-list-task--dragging");
-        const addTaskBtn = container.parentNode.querySelector('.workspace__add-task-btn');
+        const addTaskBtn = container.parentNode.querySelector(
+            ".workspace__add-task-btn"
+        );
         if (afterElement == null) {
             container.parentNode.insertBefore(draggable, addTaskBtn);
         } else {
             container.insertBefore(draggable, afterElement);
         }
-
     } else {
         let draggable = $(".workspace__board-list-task--dragging");
-        container = event.target.querySelector('.workspace__board-list-scrollable')
+        container = event.target.querySelector(
+            ".workspace__board-list-scrollable"
+        );
         if (draggable != null && container != null) {
-            container.appendChild(draggable)
+            container.appendChild(draggable);
         }
-
     }
 }
 
