e068a20c54fa9855fc4775222436d951749a2ee7
Tran Mai Nhung <nhungmaitran1412@gmail.com>
2022-04-13 20:47:41 +0700

    fix design homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -66,16 +66,16 @@
 //     scrollBar:true
 // });
 
-document.querySelector(".features__card").addEventListener('scroll', event => {
-        var scrollTop = document.querySelector("features__card").scrollTop;
-        var scrollHeight = document.querySelector("features__card").scrollHeight; // added
-        var offsetHeight = document.querySelector("features__card").offsetHeight;
-        // var clientHeight = document.querySelector("features__card").clientHeight;
-        var contentHeight = scrollHeight - offsetHeight; // added
-        if (contentHeight <= scrollTop) { // modified 
-            // Now this is called when scroll end!
-            console.log("scroll end");
-        }
-    },
-    false
-);
+// document.querySelector(".features__card").addEventListener('scroll', event => {
+//         var scrollTop = document.querySelector("features__card").scrollTop;
+//         var scrollHeight = document.querySelector("features__card").scrollHeight; // added
+//         var offsetHeight = document.querySelector("features__card").offsetHeight;
+//         // var clientHeight = document.querySelector("features__card").clientHeight;
+//         var contentHeight = scrollHeight - offsetHeight; // added
+//         if (contentHeight <= scrollTop) { // modified 
+//             // Now this is called when scroll end!
+//             console.log("scroll end");
+//         }
+//     },
+//     false
+// );
