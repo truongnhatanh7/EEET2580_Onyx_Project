5eb5ba11fa35a271a03831971ba42bc9dfbd4a4a
Tran Mai Nhung <nhungmaitran1412@gmail.com>
2022-04-14 18:09:37 +0700

    fixed

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,16 +1,3 @@
-document.querySelector(".features__card").addEventListener('scroll', event => {
-        var scrollTop = document.querySelector("features__card").scrollTop;
-        var scrollHeight = document.querySelector("features__card").scrollHeight; // added
-        var offsetHeight = document.querySelector("features__card").offsetHeight;
-        // var clientHeight = document.querySelector("features__card").clientHeight;
-        var contentHeight = scrollHeight - offsetHeight; // added
-        if (contentHeight <= scrollTop) { // modified 
-            // Now this is called when scroll end!
-            console.log("scroll end");
-        }
-    },
-    false
-);
 // localStorage.setItem("login_timestamp",  new Date(2022,3,12,16,33,0))
 // console.log(localStorage.getItem("login_timestamp"));
 // console.log(new Date(localStorage.getItem("login_timestamp")))
