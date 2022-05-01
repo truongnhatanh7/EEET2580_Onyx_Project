8f5e5ac2dcebc47408f8a334cd08c38fabb3175c
Tran Mai Nhung <nhungmaitran1412@gmail.com>
2022-05-01 11:31:02 +0700

    fix while searching web still displays available open detail + add hover effect + clean code in js

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -11,6 +11,7 @@ async function animationDetails() {
     });
 }
 async function getFaqContent() {
+    // fetch json and create element
     const response = await (await fetch("http://localhost:5501/assets/js/faq.json")).json();
     response.map(card => {
         faqContainer.innerHTML += `
@@ -19,15 +20,16 @@ async function getFaqContent() {
         
         <div class="faq__card-content flex flex-center">
         <p>${card["detail"].reduce((totalLine, line, index) => {
-            return totalLine + (index === 0? (line) : ("<br>" + line));
+            return totalLine + (index === 0? ("- " + line) : ("<br> - " + line));
         }, "")}</p>
 
-                    ${card["img"] == ""? "" : `<img src="${card["img"]}" alt="Setup in faq">`}
-                </div>
-            </details>`;
+        ${card["img"] == ""? "" : `<img src="${card["img"]}" alt="Setup in faq">`}
+        </div>
+        </details>`;
     });
 
 }
+// for animation on details + close other details when open 1 detail
 class DetailObject {
     constructor(element) {
       // detail
@@ -39,69 +41,71 @@ class DetailObject {
       // content p
       this.content = element.querySelector('.faq__card-content');
   
-      // Store the animation object (so we can cancel it if needed)
+      // store the animation state (can cancel if needed)
       this.animation = null;
-      // Store if the element is closing
+      // store closing state
       this.isClosing = false;
-      // Store if the element is expanding
+      // store expanding state
       this.isExpanding = false;
-      // Detect user clicks on the summary element
+      // check if user click summary (to expand or close)
       this.summary.addEventListener('click', async (event) => {
-          await this.closeOtherDetails(this.element);
-          this.onClick(event);
+            //   close other details before display animation on detail
+            await this.closeOtherDetails(this.element);
+            // display animation
+            this.onClick(event);
         });
     }
   
     onClick(event) {
-      // Stop default behaviour from the browser
+      // stop default behavior from browser
       event.preventDefault();
-      // Add an overflow on the <details> to avoid content overflowing
+      // add an overflow on the <details> to avoid content overflowing (after closing, content still appear)
       this.element.style.overflow = 'hidden';
-      // Check if the element is being closed or is already closed
+      // check if the element is being closed (during animation) or is already closed
       if (this.isClosing || !this.element.open) {
         this.open();
-      // Check if the element is being openned or is already open
+      // check if the element is being openned or is already open
       } else if (this.isExpanding || this.element.open) {
         this.shrink();
       }
     }
   
     shrink() {
-      // Set the element as "being closed"
+      // set the element as "being closed"
       this.isClosing = true;
       
-      // Store the current height of the element
+      // store the current height of the element
       const startHeight = `${this.element.offsetHeight}px`;
-      // Calculate the height of the summary
+      // calculate the height of the summary
       const endHeight = `${this.summary.offsetHeight}px`;
       
-      // If there is already an animation running
+      // if there is already an animation running
       if (this.animation) {
-        // Cancel the current animation
+        // cancel the current animation
         this.animation.cancel();
       }
       
-      // Start a WAAPI animation
+      // animation
       this.animation = this.element.animate({
-        // Set the keyframes from the startHeight to endHeight
+        // keyframes from start to end height
         height: [startHeight, endHeight]
       }, {
         duration: 400,
         easing: 'ease-out'
       });
       
-      // When the animation is complete, call onAnimationFinish()
+      // when the animation is complete
       this.animation.onfinish = () => this.onAnimationFinish(false);
-      // If the animation is cancelled, isClosing variable is set to false
+      // if animation is cancelled, isClosing false
       this.animation.oncancel = () => this.isClosing = false;
     }
   
     open() {
       // Apply a fixed height on the element
       this.element.style.height = `${this.element.offsetHeight}px`;
-      // Force the [open] attribute on the details element
+      // force the [open] attribute on the details element
       this.element.open = true;
-      // Wait for the next frame to call the expand function
+      // wait for the next frame to call the expand function
       window.requestAnimationFrame(() => this.expand());
     }
   
@@ -157,13 +161,12 @@ function filterFaqCard() {
     let input = document.querySelector(".faq__find");
     let filter = input.value.toUpperCase();
     let summaryList = document.querySelectorAll(".faq__card summary");
-    console.log(summaryList);
     summaryList.forEach(summary => {
         summaryContent = summary.textContent || summary.innerText;
         if (summaryContent.toUpperCase().indexOf(filter) > -1) {
-            summary.style.display = "";
+            summary.parentElement.style.display = "";
         } else {
-            summary.style.display = "none";
+            summary.parentElement.style.display = "none";
         }
     });
 }
