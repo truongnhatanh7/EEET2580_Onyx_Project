bebfbe4185df7a65f2296427e51bdfbf37dee6f3
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-05-08 16:38:27 +0700

    Update avatar.js

--- app/assets/js/avatar.js
+++ app/assets/js/avatar.js
@@ -1,11 +1,10 @@
 const userTaskWrapper = $(".user-task__wrapper");
 const userAvatar = $(".user-avatar");
-function renderUserNavbar() {
-    console.log("yes")
-}
+
 
 userAvatar.onerror = () => {
     userAvatar.classList.add('disable');
+    avatar.classList.remove('disable');
 }
 
 const avatar = $(".user-text-avatar")
@@ -16,15 +15,12 @@ userTaskWrapper.addEventListener('click', () => {
 function renderAvatarFromName() {
 
         userAvatar.classList.add("disable")
-        console.log("SDF")
         let nameList = sessionStorage.getItem("userName").split(" ");
         let processedName = nameList[0][0] + nameList[nameList.length - 1][0];
         avatar.innerText = processedName.toUpperCase();
-
         userAvatar.classList.remove("disable")
         userAvatar.src = sessionStorage.getItem("userAvatar")
     
 }
 
-renderUserNavbar()
 renderAvatarFromName()
