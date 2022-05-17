432535b1eae18c348137113e70b599369763456e
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-05-17 18:49:12 +0700

    Update workspace_status.js

--- app/assets/js/workspace_status.js
+++ app/assets/js/workspace_status.js
@@ -86,7 +86,6 @@ const addCollaboratorInput = $(".workspace__add-collaborator-input");
 
 fetchUserInWorkspace();
 function fetchOwner() {
-
     fetch("http://localhost:8080/api/v1/workspace/get-owner/" + sessionStorage.getItem("currentBoardId"))
     .then(response => response.json())
     .then(data => {
@@ -102,21 +101,24 @@ function fetchOwner() {
 
 
 function fetchUserInWorkspace() {
-    fetchOwner();
-    let workspaceUrl =
-        "http://localhost:8080/api/v1/workspace/get-workspace/" +
-        sessionStorage.getItem("currentBoardId");
-    fetch(workspaceUrl)
-        .then((response) => response.json())
-        .then((workspace) => {
-            return workspace.users;
-        })
-        .then((users) => {
-            renderUserInWorkspace(users);
-        })
-        .catch(() => {
-            throwError("Unexpected error, cannot fetch collaborator")
-        })
+    return new Promise((resolve, reject) => {
+        fetchOwner();
+    }).then(() => {
+        let workspaceUrl =
+            "http://localhost:8080/api/v1/workspace/get-workspace/" +
+            sessionStorage.getItem("currentBoardId");
+        fetch(workspaceUrl)
+            .then((response) => response.json())
+            .then((workspace) => {
+                return workspace.users;
+            })
+            .then((users) => {
+                renderUserInWorkspace(users);
+            })
+            .catch(() => {
+                throwError("Unexpected error, cannot fetch collaborator")
+            })
+    })
 }
 
 function renderUserInWorkspace(users) {
